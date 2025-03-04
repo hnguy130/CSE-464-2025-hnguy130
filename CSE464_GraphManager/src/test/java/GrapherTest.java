247e56d6641fc4be9b6a97142b091d3c7737bb89
hnguy130 <hnguy130@asu.edu>
2025-03-03 17:33:46 -0700

    Create GrapherTest.java containing all Junit tests to be compiled
    Tests for feature 1, feature 2, feature 3, feature 4_DOT, feature4_PNG

---
+++ CSE464_GraphManager/src/test/java/GrapherTest.java
@@ -0,0 +1,123 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class GrapherTest {
+	public Grapher grapher;
+
+	@Before
+	public void setup() {
+		grapher = new Grapher();
+	}
+
+	@Test
+	public void feature1() throws IOException {
+		System.out.println("\n-----------------");
+		System.out.println("feature1 test");
+		System.out.println("-----------------\n");
+		String test = "test.txt";
+		String output = "output1.txt";
+		grapher.parseGraph(test);
+		
+		grapher.outputGraph(output);
+
+		String result = Files.readString(Paths.get("output1.txt"));
+		String expected = Files.readString(Paths.get("expected1.txt"));
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void feature2() throws IOException {
+		System.out.println("\n-----------------");
+		System.out.println("feature2 test");
+		System.out.println("-----------------\n");
+		String test = "test.txt";
+		String output = "output2.txt";
+		grapher.parseGraph(test);
+		
+		grapher.addNode("a");
+		grapher.addNode("b");
+		grapher.addNode("m");
+		String[] nodes = {"a","z","y"};
+		grapher.addNodes(nodes);
+		
+		grapher.outputGraph(output);
+		String result = Files.readString(Paths.get("output2.txt"));
+		String expected = Files.readString(Paths.get("expected2.txt"));
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void feature3() throws IOException {
+		System.out.println("\n-----------------");
+		System.out.println("feature3 test");
+		System.out.println("-----------------\n");
+		String test = "test.txt";
+		String output = "output3.txt";
+		grapher.parseGraph(test);
+		
+		grapher.addEdge("a", "b");
+		grapher.addEdge("d", "e");
+		grapher.addEdge("d", "f");
+		grapher.addEdge("x", "y");
+		
+		grapher.outputGraph(output);
+		String result = Files.readString(Paths.get("output3.txt"));
+		String expected = Files.readString(Paths.get("expected3.txt"));
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void feature4_DOT() throws IOException {
+		System.out.println("\n-----------------");
+		System.out.println("feature4_DOT test");
+		System.out.println("-----------------\n");
+		String test = "test.txt";
+		String output = "output4.txt";
+		grapher.parseGraph(test);
+		
+		grapher.addNode("a");
+		grapher.addNode("b");
+		grapher.addNode("m");
+		String[] nodes = {"a","z","y"};
+		grapher.addNodes(nodes);
+		
+		grapher.addEdge("a", "b");
+		grapher.addEdge("d", "e");
+		grapher.addEdge("d", "f");
+		grapher.addEdge("x", "y");
+		
+		grapher.outputDOTGraph(output);
+		String result = Files.readString(Paths.get("output4.txt"));
+		String expected = Files.readString(Paths.get("expected4.txt"));
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void feature4_PNG() throws IOException {
+		System.out.println("\n-----------------");
+		System.out.println("feature4_PNG test");
+		System.out.println("-----------------\n");
+		String test = "test.txt";
+		String output = "output.png";
+		grapher.parseGraph(test);
+		
+		grapher.addNode("a");
+		grapher.addNode("b");
+		grapher.addNode("m");
+		String[] nodes = {"a","z","y"};
+		grapher.addNodes(nodes);
+		
+		grapher.addEdge("a", "b");
+		grapher.addEdge("d", "e");
+		grapher.addEdge("d", "f");
+		grapher.addEdge("x", "y");
+		
+		grapher.outputGraphics(output);
+		assertTrue(Files.exists(Paths.get("output.png")));
+	}
+}
